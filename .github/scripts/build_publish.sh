ea225617d282a9a61d5f5b3ec523f1768c1af3f4
Chris Soyars <ctsoyars@gmail.com>
2020-09-20 09:46:12 -0700

    Switch to GITHUB_TOKEN

--- .github/scripts/build_publish.sh
+++ .github/scripts/build_publish.sh
@@ -4,7 +4,7 @@ shortSHA=$(echo "${GITHUB_SHA}" | cut -c1-7)
 BASE_NAME="docker.pkg.github.com/${GITHUB_REPOSITORY}/app"
 SHA_NAME="${BASE_NAME}:${shortSHA}"
 
-echo ${GITHUB_PACKAGE_TOKEN} | docker login -u ctso --password-stdin docker.pkg.github.com
+echo ${GITHUB_TOKEN} | docker login -u ctso --password-stdin docker.pkg.github.com
 
 # Build container
 docker build -t ${SHA_NAME} -t ${BASE_NAME} -f Dockerfile .
